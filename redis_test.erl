a5beac2688eb6887c8b0745c81c3b75ae195ba1e
Yuta Imai <imai.factory@gmail.com>
2013-09-30 10:52:42 +0000

    first commit

---
+++ redis_test.erl
@@ -0,0 +1,71 @@
+-module(redis_test).
+
+-define(PRECISION,10).
+
+-export([start/4]).
+
+start(WriteProcess,ReadProcess, Count, Redis) ->
+    {ok, Pid} = eredis:start_link(Redis,6379),    
+    eredis:q(Pid,["flushall"]),
+
+    parallel_set(WriteProcess, Count, Pid),
+    parallel_get(ReadProcess, Count, Pid),
+    ProcessCount = WriteProcess + ReadProcess,
+    collect_loop(ProcessCount).
+
+   
+get_seconds() ->
+    {Mega, Sec, Micro} = now(),
+    ((Mega * 1000000 + Sec) * 1000000 + Micro) / 1000000.
+    
+
+parallel_get(0, _Count, _Pid) -> ok;
+parallel_get(ProcessCount, Count, Pid) ->
+    ParentPid = self(),
+    spawn(fun() ->
+                get_key(Count, Pid),
+                ParentPid ! ok
+          end),
+    parallel_get(ProcessCount - 1, Count, Pid).
+
+get_key(0, _Pid) -> ok;
+get_key(Count,Pid) ->
+    StartTime = get_seconds(),
+    eredis:q(Pid,['set'|[Count,"1"]]),
+    EndTime = get_seconds(),
+    Flag = judge(Count),
+    output("Read",Flag, EndTime - StartTime),
+    get_key(Count -1, Pid).
+
+
+parallel_set(0, _Count, _Pid) -> ok;
+parallel_set(ProcessCount, Count, Pid) ->
+    ParentPid = self(),
+    spawn(fun() ->
+                set_key(Count, Pid),
+                ParentPid ! ok
+          end),
+    parallel_set(ProcessCount - 1, Count, Pid).
+
+set_key(0, _Pid) -> ok;
+set_key(Count,Pid) ->
+    StartTime = get_seconds(),
+    eredis:q(Pid,['set'|[Count,"1"]]),
+    EndTime = get_seconds(),
+    Flag = judge(Count),
+    output("Write",Flag, EndTime - StartTime),
+    set_key(Count -1, Pid).
+
+judge(Count) -> Count rem ?PRECISION.
+
+output(Operation,0,Time) ->
+    {{Year, Month, Day}, {Hour, Min, _Sec}} =  calendar:local_time(),
+    catch io:format("~w-~w-~w ~w:~w,~s,~w~n",[Year,Month,Day,Hour,Min,Operation,Time * 1000]);
+output(_Operation,_Count,_Time) -> ok.
+    
+    
+collect_loop(0) -> ok;
+collect_loop(Count) ->
+    receive
+        ok -> collect_loop(Count - 1)
+    end.
